7784d1fb45cd6e5898527d779a1a6883b137cc92
Bruno Lemos <brunohplemos@gmail.com>
2019-07-25 14:28:19 -0300

    Fix for when a tweet is opened as a new tab in backgroun

--- chrome/index.js
+++ chrome/index.js
@@ -1,14 +1,16 @@
 function script() {
-  window.onresize = e => {
-    window.innerWidth = Math.min(e.target.outerWidth, 800)
-
-    document.body.style.paddingLeft =
-      e.target.innerWidth >= 800 ? '176px' : null
+  function update(win = window) {
+    window.innerWidth = Math.min(win.outerWidth, 800)
+    document.body.style.paddingLeft = win.innerWidth >= 800 ? '176px' : null
   }
 
-  window.onload = () => {
-    window.dispatchEvent(new Event('resize'))
+  function triggerUpdate() {
+    setTimeout(() => window.dispatchEvent(new Event('resize')), 1)
   }
+
+  window.onresize = e => update(e.target || window)
+  window.onload = triggerUpdate
+  document.addEventListener('visibilitychange', triggerUpdate)
 }
 
 function inject(fn) {
