a1023c9f3e8b1cdbb0b67cf81704d77385ca57bc
Bruno Lemos <brunohplemos@gmail.com>
2019-07-26 04:01:15 -0300

    Firefox extension
    Closes #1

---
+++ firefox/inject.js
@@ -0,0 +1,7 @@
+;(function() {
+  const b = typeof browser !== 'undefined' ? browser : chrome
+
+  const script = document.createElement('script')
+  script.src = b.runtime.getURL('script.js')
+  document.documentElement.appendChild(script)
+})()
