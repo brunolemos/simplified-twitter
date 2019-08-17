2642b05953f2ebfa2987a6dd77cd4233bc9eba1f
Bruno Lemos <brunohplemos@gmail.com>
2019-08-17 20:57:09 -0300

    Wrap on an IIFE to avoid variable name conflict

--- chrome/index.js
+++ chrome/index.js
@@ -1,22 +1,24 @@
-function script() {
-  function update(win = window) {
-    window.innerWidth = Math.min(win.outerWidth, 800)
-    document.body.style.paddingLeft = win.innerWidth >= 800 ? '176px' : null
+;(function() {
+  function script() {
+    function update(win = window) {
+      window.innerWidth = Math.min(win.outerWidth, 800)
+      document.body.style.paddingLeft = win.innerWidth >= 800 ? '176px' : null
+    }
+
+    function triggerUpdate() {
+      setTimeout(() => window.dispatchEvent(new Event('resize')), 1)
+    }
+
+    window.onresize = e => update(e.target || window)
+    window.onload = triggerUpdate
+    document.addEventListener('visibilitychange', triggerUpdate)
   }
 
-  function triggerUpdate() {
-    setTimeout(() => window.dispatchEvent(new Event('resize')), 1)
+  function inject(fn) {
+    const script = document.createElement('script')
+    script.text = `(${fn.toString()})();`
+    document.documentElement.appendChild(script)
   }
 
-  window.onresize = e => update(e.target || window)
-  window.onload = triggerUpdate
-  document.addEventListener('visibilitychange', triggerUpdate)
-}
-
-function inject(fn) {
-  const script = document.createElement('script')
-  script.text = `(${fn.toString()})();`
-  document.documentElement.appendChild(script)
-}
-
-inject(script)
+  inject(script)
+})()
