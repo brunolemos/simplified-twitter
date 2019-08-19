67b93cf5f6564339cea2a0655660a67e14c098df
bcye <br.apps@icloud.com>
2019-08-19 14:27:00 +0200

    init commit for safari

---
+++ Simplified Twitter/Simplified Twitter/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Simplified Twitter
+//
+//  Created by Bruce Roettgers on 19.08.19.
+//  Copyright © 2019 Dirk Hulverscheidt. All rights reserved.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = "Simplified Twitter";
+    }
+    
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "hulverscheidtdirk.Simplified-Twitter-Extension") { error in
+            if let _ = error {
+                // Insert code to inform the user that something went wrong.
+
+            }
+        }
+    }
+
+}
