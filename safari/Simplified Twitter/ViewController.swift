37e88014ce25073333b1ada202e49e681d0f2524
Bruno Lemos <brunohplemos@gmail.com>
2019-09-07 19:05:50 -0300

    [Safari] Update sigining team and bundle id

--- safari/Simplified Twitter/ViewController.swift
+++ safari/Simplified Twitter/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: NSViewController {
     }
     
     @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: "hulverscheidtdirk.Simplified-Twitter-Extension") { error in
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "org.brunolemos.simplifiedtwitter.extension") { error in
             if let _ = error {
                 // Insert code to inform the user that something went wrong.
 
