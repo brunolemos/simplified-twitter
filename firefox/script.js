a9f25b26e45838977e1c83fc553f050791080fc7
Bruno Lemos <brunohplemos@gmail.com>
2020-04-25 21:20:14 -0300

    Inject css to center content before dom elements be ready

--- firefox/script.js
+++ firefox/script.js
@@ -1,18 +1,6 @@
 ;(function () {
-  function update(win = window, retry = 3) {
+  function update(win = window) {
     window.innerWidth = Math.min(win.outerWidth, 800)
-
-    try {
-      document
-        .querySelector('main')
-        .parentElement.querySelector('header').style.flexGrow =
-        win.innerWidth >= 800 ? 2 : null
-      document.body.style.paddingLeft = null
-    } catch (error) {
-      document.body.style.paddingLeft = win.innerWidth >= 800 ? '188px' : null
-
-      if (retry) setTimeout(() => update(win, retry - 1), 1000)
-    }
   }
 
   function triggerUpdate() {
