59cd155dddaec93e8669621e2658b42e22088043
Bruno Lemos <brunohplemos@gmail.com>
2021-06-08 15:08:37 -0300

    Fix extension after Twitter update
    Twitter bumbed react-native-web which changed how Dimensions work
    
    https://github.com/necolas/react-native-web/commit/89cf78d529d2a1e46a247b27c9a0a869416f440f
    
    https://github.com/necolas/react-native-web/releases/tag/0.16.0

--- chrome/index.js
+++ chrome/index.js
@@ -1,10 +1,12 @@
 ;(function () {
   function script() {
     function update() {
-      const innerWidth = Math.min(window.outerWidth || 800, 800)
-      if (window.innerWidth === innerWidth) return
+      const width = Math.min(window.outerWidth || 800, 800)
+      if (window.innerWidth === width && document.documentElement.clientWidth === width) return
 
-      window.innerWidth = innerWidth
+      window.__defineGetter__('innerWidth', () => width)
+      document.documentElement.__defineGetter__('clientWidth', () => width)
+
       window.dispatchEvent(new Event('resize'))
     }
 
--- firefox/script.js
+++ firefox/script.js
@@ -1,9 +1,11 @@
 ;(function () {
   function update() {
-    const innerWidth = Math.min(window.outerWidth || 800, 800)
-    if (window.innerWidth === innerWidth) return
+    const width = Math.min(window.outerWidth || 800, 800)
+    if (window.innerWidth === width && document.documentElement.clientWidth === width) return
 
-    window.innerWidth = innerWidth
+    window.__defineGetter__('innerWidth', () => width)
+    document.documentElement.__defineGetter__('clientWidth', () => width)
+
     window.dispatchEvent(new Event('resize'))
   }
 
